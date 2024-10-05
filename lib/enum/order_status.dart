91a3bd9683c2946b7a88855f14be9cb7520ad5e6
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-10-06 01:47:10 +0530

    add new method to convert string to enum

--- lib/enum/order_status.dart
+++ lib/enum/order_status.dart
@@ -1,3 +1,18 @@
-enum OrderStatus{
-  Pending,Preparing,Completed
+enum OrderStatus { Pending, Preparing, Completed, Editing, Paid }
+
+OrderStatus getOrderStatusFromString(String status) {
+  switch (status) {
+    case 'Pending':
+      return OrderStatus.Pending;
+    case 'Preparing':
+      return OrderStatus.Preparing;
+    case 'Completed':
+      return OrderStatus.Completed;
+    case 'Editing':
+      return OrderStatus.Editing;
+    case 'Paid':
+      return OrderStatus.Paid;
+    default:
+      throw Exception('Invalid order status: $status');
+  }
 }
