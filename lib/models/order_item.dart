1b3205eaeb5d7e136d19316e3d99b66a341098f9
AshenSandeep <129169964+AshenSandeep@users.noreply.github.com>
2024-10-06 18:20:22 +0530

    error getting data from the backend is solved.

--- lib/models/order_item.dart
+++ lib/models/order_item.dart
@@ -31,15 +31,31 @@ class OrderItem {
       'addonList': addonList.map((e) => e.toString()).toList(),
     };
   }
+  // factory OrderItem.fromMap(Map<String, dynamic> map) {
+  //   return OrderItem(
+  //     orderItemId: map['orderItemId'],
+  //     quantity: map['quantity'],
+  //     specialNote: map['specialNote'],
+  //     totalPrice: map['totalPrice'].toDouble(),
+  //     menuItem: MenuItem.fromMap(map['menuItem']), // Map the nested menuItem
+  //     addonList: map['selectedAddOns'] != null
+  //         ? List<int>.from(map['selectedAddOns']) // Assuming add-ons are integers
+  //         : [],
+  //   );
+  // }
+
+  //after changing the db by sunera.
   factory OrderItem.fromMap(Map<String, dynamic> map) {
     return OrderItem(
       orderItemId: map['orderItemId'],
       quantity: map['quantity'],
       specialNote: map['specialNote'],
       totalPrice: map['totalPrice'].toDouble(),
-      menuItem: MenuItem.fromMap(map['menuItem']), // Map the nested menuItem
+      menuItem: MenuItem.fromMap(map['menuItem']),
+      // Assuming that selectedAddOns contains objects with 'addOn' inside them
       addonList: map['selectedAddOns'] != null
-          ? List<int>.from(map['selectedAddOns']) // Assuming add-ons are integers
+          ? List<int>.from(map['selectedAddOns']
+          .map((addOn) => addOn['addOn']['addOnId'])) // Extract 'addOnId'
           : [],
     );
   }
